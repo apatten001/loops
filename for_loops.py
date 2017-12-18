c70b3ea7be0607ec4fa4084d5a69bf80e6af2701
Arnold <apatten001@yahoo.com>
2017-12-18 14:59:34 -0500

    Add files via upload

---
+++ for_loops.py
@@ -0,0 +1,23 @@
+
+# for loops allow us to loop through something a set number of times
+
+for x in range(10):
+    print(x)
+
+
+# you can loop through list as well
+
+var_1  = "Tiles"
+list_1 = [21,54,76,90,"Arnold", var_1]
+
+
+for y in list_1:
+    print(y)
+
+# the variable in the for loop can be anything
+
+# the third argument allows you to skip by when you are using range
+for pizza in range(1,24,2):
+    print(pizza)
+
+
